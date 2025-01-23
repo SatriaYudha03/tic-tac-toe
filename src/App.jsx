eaba5db91a05227fb9c73166f89965b256c79194
Satria Yudha <satriayuda2017@gmail.com>
2025-01-23 17:18:42 +0800

    calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true)
 
   function handleClick(i) {
-    if (squares[i]) return
+    if (squares[i] || calculateWinner(squares)) return
 
     const nextSquares = squares.slice();
 
@@ -18,7 +18,17 @@ export default function Board() {
     setXIsNext(!xIsNext)
   }
 
+const winner = calculateWinner(squares)
+let status = '';
+if (winner) {
+  status = 'Winner: ' + winner; 
+} else {
+  status = 'Next Player: ' + (xIsNext ? 'X' : 'O')
+}
+
   return (
+    <>
+    <div className="status">{status}</div>
     <div className="board">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
       <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -30,5 +40,28 @@ export default function Board() {
       <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
       <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
+    </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  for(let i=0; i< lines.length; i++){
+    const [a,b,c] = lines[i];
+
+    if(squares[a] && squares[a] === squares[b] && squares[c]) {
+      return squares[a]
+    }
+  }
+  return false
 }
